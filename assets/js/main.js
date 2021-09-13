db35f7b9c3759590643ed0c04e7775ff8152e27d
taisei-ikeda <89883345+taisei-ikeda@users.noreply.github.com>
2021-09-13 10:47:57 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'JAMAMONO',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'めもりー',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'きつね',
           // q2: 'かかか',
           // q3: 'ききき',
         }
